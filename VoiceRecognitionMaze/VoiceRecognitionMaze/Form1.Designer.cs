8defd8a768ffdac908c467964e64eb71336d3fc5
MeliMolina <melimolina04@hotmail.com>
2018-03-27 19:44:40 -0600

    Implementacion Tablero
    Falta que el tamaño "a" se le pueda poner a las casillas

--- VoiceRecognitionMaze/VoiceRecognitionMaze/Form1.Designer.cs
+++ VoiceRecognitionMaze/VoiceRecognitionMaze/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.matrizTablero = new System.Windows.Forms.DataGridView();
             this.BtnIniciar = new System.Windows.Forms.Button();
             this.TxbColumnas = new System.Windows.Forms.TextBox();
             this.TxbFilas = new System.Windows.Forms.TextBox();
@@ -39,6 +39,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.TxbTamano = new System.Windows.Forms.TextBox();
             this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.matrizTablero = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.matrizTablero)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,25 +49,10 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // matrizTablero
-            // 
-            this.matrizTablero.AllowUserToAddRows = false;
-            this.matrizTablero.AllowUserToDeleteRows = false;
-            this.matrizTablero.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.matrizTablero.ColumnHeadersVisible = false;
-            this.matrizTablero.Location = new System.Drawing.Point(295, 33);
-            this.matrizTablero.Margin = new System.Windows.Forms.Padding(2);
-            this.matrizTablero.Name = "matrizTablero";
-            this.matrizTablero.ReadOnly = true;
-            this.matrizTablero.RowHeadersVisible = false;
-            this.matrizTablero.RowTemplate.Height = 28;
-            this.matrizTablero.Size = new System.Drawing.Size(332, 285);
-            this.matrizTablero.TabIndex = 1;
-            // 
             // BtnIniciar
             // 
             this.BtnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnIniciar.Location = new System.Drawing.Point(29, 25);
+            this.BtnIniciar.Location = new System.Drawing.Point(11, 167);
             this.BtnIniciar.Margin = new System.Windows.Forms.Padding(2);
             this.BtnIniciar.Name = "BtnIniciar";
             this.BtnIniciar.Size = new System.Drawing.Size(69, 29);
@@ -77,25 +63,25 @@
             // 
             // TxbColumnas
             // 
-            this.TxbColumnas.Location = new System.Drawing.Point(55, 66);
+            this.TxbColumnas.Location = new System.Drawing.Point(33, 47);
             this.TxbColumnas.Margin = new System.Windows.Forms.Padding(2);
             this.TxbColumnas.Name = "TxbColumnas";
-            this.TxbColumnas.Size = new System.Drawing.Size(68, 20);
+            this.TxbColumnas.Size = new System.Drawing.Size(44, 20);
             this.TxbColumnas.TabIndex = 3;
             // 
             // TxbFilas
             // 
-            this.TxbFilas.Location = new System.Drawing.Point(55, 93);
+            this.TxbFilas.Location = new System.Drawing.Point(33, 83);
             this.TxbFilas.Margin = new System.Windows.Forms.Padding(2);
             this.TxbFilas.Name = "TxbFilas";
-            this.TxbFilas.Size = new System.Drawing.Size(68, 20);
+            this.TxbFilas.Size = new System.Drawing.Size(44, 20);
             this.TxbFilas.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 66);
+            this.label1.Location = new System.Drawing.Point(11, 47);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 20);
@@ -106,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(26, 93);
+            this.label2.Location = new System.Drawing.Point(11, 81);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 20);
@@ -117,7 +103,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(27, 120);
+            this.label3.Location = new System.Drawing.Point(11, 120);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(18, 20);
@@ -126,28 +112,54 @@
             // 
             // TxbTamano
             // 
-            this.TxbTamano.Location = new System.Drawing.Point(55, 122);
+            this.TxbTamano.Location = new System.Drawing.Point(33, 122);
             this.TxbTamano.Margin = new System.Windows.Forms.Padding(2);
             this.TxbTamano.Name = "TxbTamano";
-            this.TxbTamano.Size = new System.Drawing.Size(68, 20);
+            this.TxbTamano.Size = new System.Drawing.Size(44, 20);
             this.TxbTamano.TabIndex = 7;
             // 
             // BtnLimpiar
             // 
             this.BtnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnLimpiar.Location = new System.Drawing.Point(113, 25);
+            this.BtnLimpiar.Location = new System.Drawing.Point(11, 210);
             this.BtnLimpiar.Margin = new System.Windows.Forms.Padding(2);
             this.BtnLimpiar.Name = "BtnLimpiar";
             this.BtnLimpiar.Size = new System.Drawing.Size(69, 29);
             this.BtnLimpiar.TabIndex = 8;
             this.BtnLimpiar.Text = "Limpiar";
             this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // matrizTablero
+            // 
+            this.matrizTablero.AllowUserToAddRows = false;
+            this.matrizTablero.AllowUserToDeleteRows = false;
+            this.matrizTablero.AllowUserToResizeColumns = false;
+            this.matrizTablero.AllowUserToResizeRows = false;
+            this.matrizTablero.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizTablero.ColumnHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.matrizTablero.DefaultCellStyle = dataGridViewCellStyle2;
+            this.matrizTablero.Location = new System.Drawing.Point(101, 12);
+            this.matrizTablero.Name = "matrizTablero";
+            this.matrizTablero.ReadOnly = true;
+            this.matrizTablero.RowHeadersVisible = false;
+            this.matrizTablero.Size = new System.Drawing.Size(1106, 632);
+            this.matrizTablero.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(649, 481);
+            this.ClientSize = new System.Drawing.Size(1219, 656);
+            this.Controls.Add(this.matrizTablero);
             this.Controls.Add(this.BtnLimpiar);
             this.Controls.Add(this.TxbTamano);
             this.Controls.Add(this.label3);
@@ -156,10 +168,11 @@
             this.Controls.Add(this.TxbFilas);
             this.Controls.Add(this.TxbColumnas);
             this.Controls.Add(this.BtnIniciar);
-            this.Controls.Add(this.matrizTablero);
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "d";
+            this.ShowIcon = false;
+            this.Text = "Maze";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.matrizTablero)).EndInit();
             this.ResumeLayout(false);
@@ -170,7 +183,6 @@
         #endregion
 
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.DataGridView matrizTablero;
         private System.Windows.Forms.Button BtnIniciar;
         private System.Windows.Forms.TextBox TxbColumnas;
         private System.Windows.Forms.TextBox TxbFilas;
@@ -179,6 +191,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox TxbTamano;
         private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.DataGridView matrizTablero;
     }
 }
 
--- VoiceRecognitionMaze/VoiceRecognitionMaze/Form1.cs
+++ VoiceRecognitionMaze/VoiceRecognitionMaze/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Recognition;
-using System.Drawing;
+
 
 namespace VoiceRecognitionMaze
 {
@@ -22,11 +22,11 @@ namespace VoiceRecognitionMaze
            
         }
 
-   
+
 
         private void BtnIniciar_Click(object sender, EventArgs e)
         {
-            try
+            /*try
             {
                 escucha.SetInputToDefaultAudioDevice();
                 escucha.LoadGrammar(new DictationGrammar());
@@ -37,8 +37,18 @@ namespace VoiceRecognitionMaze
             catch
             {
                 MessageBox.Show("Ya dio acceso al uso del micrófono");
-            }
+            }*/
+
+            InicializarTablero();
+            
+
+
+          
+          
+
         }
+
+        
 
         public void reconocedor(object sender, SpeechRecognizedEventArgs e)
         {
@@ -48,19 +58,53 @@ namespace VoiceRecognitionMaze
             }
 
 
-            int columna, fila;
-            columna = int.Parse(TxbColumnas.Text);
-            fila = int.Parse(TxbFilas.Text);
-
-            matrizTablero.ColumnCount = columna;
-            matrizTablero.RowCount = fila;
            
         }
-
-private void Form1_Load(object sender, EventArgs e)
-{
+        private void Form1_Load(object sender, EventArgs e){
            
 
         }
-}
+
+        private void InicializarTablero()
+        {
+
+            int columnas, filas, tamanio;
+            columnas = int.Parse(TxbColumnas.Text);
+            filas = int.Parse(TxbFilas.Text);
+            tamanio = int.Parse(TxbTamano.Text);
+            matrizTablero.ColumnCount = columnas;
+            matrizTablero.RowCount = filas;
+
+            matrizTablero.BackgroundColor = Color.White;
+            matrizTablero.DefaultCellStyle.BackColor = Color.White;
+
+            //Establecer el ancho de las columnas
+            foreach (DataGridViewColumn c in matrizTablero.Columns)
+            {
+              //  c.Width = matrizTablero.Width / matrizTablero.Columns.Count;
+                c.Width = tamanio;
+                c.Width += matrizTablero.Width / matrizTablero.Columns.Count;
+
+
+            }
+
+            //Establecer el ancho de las filas
+            foreach (DataGridViewRow r in matrizTablero.Rows)
+            {
+               //r.Height = matrizTablero.Height / matrizTablero.Rows.Count;
+                r.Height =  tamanio;
+                r.Height += matrizTablero.Height / matrizTablero.Rows.Count;
+
+            }
+
+
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            matrizTablero.Rows.Clear();
+            matrizTablero.Columns.Clear();
+            matrizTablero.Refresh();
+        }
+    }
 }
