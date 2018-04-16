7cd0a617f6968fc8f6a260ac22c05da37f0a65f4
Liza Chaves <lizchavca@gmail.com>
2018-04-16 00:15:39 -0600

    Arreglos gráficos y algunos comentarios

--- VoiceRecognitionMaze/VoiceRecognitionMaze/Maze.Designer.cs
+++ VoiceRecognitionMaze/VoiceRecognitionMaze/Maze.Designer.cs
@@ -29,12 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.matrizTablero = new System.Windows.Forms.DataGridView();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.palabras = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.matrizTablero)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,20 +50,21 @@
             this.matrizTablero.AllowUserToResizeRows = false;
             this.matrizTablero.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.matrizTablero.ColumnHeadersVisible = false;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(1);
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlLightLight;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.matrizTablero.DefaultCellStyle = dataGridViewCellStyle1;
-            this.matrizTablero.Location = new System.Drawing.Point(92, 9);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.matrizTablero.DefaultCellStyle = dataGridViewCellStyle2;
+            this.matrizTablero.Location = new System.Drawing.Point(13, 14);
+            this.matrizTablero.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.matrizTablero.Name = "matrizTablero";
             this.matrizTablero.ReadOnly = true;
             this.matrizTablero.RowHeadersVisible = false;
-            this.matrizTablero.Size = new System.Drawing.Size(939, 577);
+            this.matrizTablero.Size = new System.Drawing.Size(1252, 852);
             this.matrizTablero.TabIndex = 9;
             // 
             // timer1
@@ -74,35 +73,14 @@
             this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 120);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Limpiar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // palabras
-            // 
-            this.palabras.Location = new System.Drawing.Point(12, 424);
-            this.palabras.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.palabras.Name = "palabras";
-            this.palabras.Size = new System.Drawing.Size(68, 20);
-            this.palabras.TabIndex = 11;
-            // 
             // Maze
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1034, 590);
-            this.Controls.Add(this.palabras);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1289, 882);
             this.Controls.Add(this.matrizTablero);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Maze";
             this.ShowIcon = false;
@@ -110,7 +88,6 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.matrizTablero)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -119,8 +96,6 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.DataGridView matrizTablero;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox palabras;
     }
 }
 
