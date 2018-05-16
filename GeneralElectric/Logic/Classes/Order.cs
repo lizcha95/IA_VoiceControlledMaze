413895004f93e420ac380559dce7786752a302ab
ARTINSOFT\davargas <davargas@mobilize.net>
2018-05-15 20:07:15 -0600

    An incomplete version of the GeneticAllocator.Crossover has been implemented.

--- GeneralElectric/Logic/Classes/Order.cs
+++ GeneralElectric/Logic/Classes/Order.cs
@@ -1,10 +1,13 @@
 ﻿namespace Logic.Classes
 {
+    using System.Collections.Generic;
+
     public class Order
     {
         private int id;
         private string client;
         private Service service;
+        private static OrderComparer comparer = new OrderComparer();
 
         public Order(int id, string client, Service service)
         {
@@ -16,5 +19,20 @@
         public int ID { get { return this.id; } }
         public string Client { get { return this.client; } }
         public Service Service { get { return this.service; } }
+
+        public static OrderComparer Comparer { get { return comparer; } }
+
+        public class OrderComparer : IEqualityComparer<Order>
+        {
+            public bool Equals(Order order1, Order order2)
+            {
+                return order1.ID == order2.ID;
+            }
+
+            public int GetHashCode(Order order)
+            {
+                return order.ID.GetHashCode();
+            }
+        }
     }
 }
--- GeneralElectric/Logic/GeneticAllocator.cs
+++ GeneralElectric/Logic/GeneticAllocator.cs
@@ -11,21 +11,29 @@
         private IEnumerable<Agent> agents;
         private IEnumerable<Service> services;
         private IEnumerable<Order> orders;
+        private IEnumerable<IGrouping<string, Order>> groupedOrders;
 
         private int agentsCount;
         private int expectedPayment;
 
-        private IEnumerable<IGrouping<string, Order>> groupedOrders;
+        private Random random;
 
         public GeneticAllocator(IEnumerable<Agent> agents, IEnumerable<Service> services, IEnumerable<Order> orders)
         {
             this.agents = agents;
             this.services = services;
             this.orders = orders;
+            this.groupedOrders = orders.GroupBy(order => order.Service.Code);
 
             this.agentsCount = agents.Count();
             this.expectedPayment = orders.Sum(order => order.Service.Payment) / this.agentsCount;
-            this.groupedOrders = orders.GroupBy(order => order.Service.Code);
+
+            this.random = new Random();
+        }
+
+        public IEnumerable<Assignment> Execute()
+        {
+            return null;
         }
 
         private IEnumerable<IEnumerable<Assignment>> InitializePopulation()
@@ -33,11 +41,11 @@
             for (int i = 0; i < Constants.Numbers.INITIAL_POPULATION; i++)
             {
                 Console.WriteLine(string.Format(Constants.Messages.GENERATING_POPULATION_MEMBER, i + 1));
-                yield return this.GeneratePopulationMember();
+                yield return this.GenerateIndividual();
             }
         }
 
-        private List<Assignment> GeneratePopulationMember()
+        public List<Assignment> GenerateIndividual()
         {
             List<Assignment> assignments = this.agents.Select(agent => new Assignment(agent)).ToList();
             List<Order> ungroupedOrders;
@@ -47,7 +55,8 @@
             {
                 temporalAssignments = assignments
                     .Where(assignment => assignment.Agent.ServicesCodes.Contains(groupedOrder.Key))
-                    .OrderByDescending(assignment => assignment.Orders.Count);
+                    .OrderByDescending(assignment => assignment.Orders.Count)
+                    .ThenByDescending(assignment => assignment.Orders.Sum(order => order.Service.Hours));
                 ungroupedOrders = groupedOrder.Select(group => group).ToList();
                 ordersQuotient = groupedOrder.Count() / temporalAssignments.Count();
                 ordersRemainder = groupedOrder.Count() % temporalAssignments.Count();
@@ -73,12 +82,26 @@
 
         }
 
-        public void Crossover()
+        public IEnumerable<Assignment> Crossover(IEnumerable<Assignment> individual1, IEnumerable<Assignment> individual2)
         {
+            // 50% chance to switch the individuals.
+            if (this.random.Next(0, 2) == 1)
+            {
+                var temporalIndividual = individual1;
+                individual1 = individual2;
+                individual2 = temporalIndividual;
+            }
+            int crossingPoint = this.random.Next(this.agentsCount / 4 - 1, this.agentsCount / 4 * 3 - 1);
+            List<Assignment> listIndividual1 = individual1.ToList(),
+                listIndividual2 = individual2.ToList(),
+                listNewIndividual = listIndividual1.GetRange(0, crossingPoint)
+                    .Concat(listIndividual2.GetRange(crossingPoint, listIndividual2.Count - crossingPoint))
+                    .ToList();
 
+            return listNewIndividual;
         }
 
-        public void Mutation()
+        public void Mutation(IEnumerable<Assignment> individual)
         {
 
         }
