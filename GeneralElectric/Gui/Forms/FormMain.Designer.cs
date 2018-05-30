9e53f6ec9265b6e9b7a50f621fe2b4f027e73ec6
MeliMolina <melimolina04@hotmail.com>
2018-05-29 21:30:08 -0600

    Paging Implementation

--- GeneralElectric/Gui/Forms/FormMain.Designer.cs
+++ GeneralElectric/Gui/Forms/FormMain.Designer.cs
@@ -29,18 +29,22 @@
         private void InitializeComponent()
         {
             this.agentsGridView = new System.Windows.Forms.DataGridView();
-            this.agentId = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.agentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.serviciosXagente = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ordersGridView = new System.Windows.Forms.DataGridView();
-            this.orderId = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.orderClient = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.orderCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.buttonLoadAgents = new System.Windows.Forms.Button();
             this.buttonAssignOrders = new System.Windows.Forms.Button();
             this.buttonLoadOrders = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_Previous_Agent = new System.Windows.Forms.Button();
+            this.button_Next_Agent = new System.Windows.Forms.Button();
+            this.labelPageNumberAgent = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button_Previous_Order = new System.Windows.Forms.Button();
+            this.button_Next_Order = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelPageNumberOrders = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.agentsGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ordersGridView)).BeginInit();
             this.SuspendLayout();
@@ -49,69 +53,29 @@
             // 
             this.agentsGridView.AllowUserToOrderColumns = true;
             this.agentsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.agentsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.agentId,
-            this.agentName,
-            this.serviciosXagente});
-            this.agentsGridView.Location = new System.Drawing.Point(24, 45);
+            this.agentsGridView.Location = new System.Drawing.Point(146, 78);
             this.agentsGridView.Margin = new System.Windows.Forms.Padding(2);
             this.agentsGridView.Name = "agentsGridView";
             this.agentsGridView.RowTemplate.Height = 28;
-            this.agentsGridView.Size = new System.Drawing.Size(667, 520);
+            this.agentsGridView.Size = new System.Drawing.Size(400, 406);
             this.agentsGridView.TabIndex = 0;
             this.agentsGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
-            // 
-            // agentId
-            // 
-            this.agentId.HeaderText = "ID";
-            this.agentId.Name = "agentId";
-            this.agentId.Width = 50;
-            // 
-            // agentName
-            // 
-            this.agentName.HeaderText = "Name";
-            this.agentName.Name = "agentName";
-            // 
-            // serviciosXagente
-            // 
-            this.serviciosXagente.HeaderText = "Services";
-            this.serviciosXagente.Name = "serviciosXagente";
             // 
             // ordersGridView
             // 
             this.ordersGridView.AllowUserToOrderColumns = true;
             this.ordersGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ordersGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.orderId,
-            this.orderClient,
-            this.orderCode});
-            this.ordersGridView.Location = new System.Drawing.Point(773, 45);
+            this.ordersGridView.Location = new System.Drawing.Point(811, 78);
             this.ordersGridView.Margin = new System.Windows.Forms.Padding(2);
             this.ordersGridView.Name = "ordersGridView";
             this.ordersGridView.RowTemplate.Height = 28;
-            this.ordersGridView.Size = new System.Drawing.Size(667, 520);
+            this.ordersGridView.Size = new System.Drawing.Size(384, 406);
             this.ordersGridView.TabIndex = 1;
-            // 
-            // orderId
-            // 
-            this.orderId.HeaderText = "ID";
-            this.orderId.Name = "orderId";
-            this.orderId.Width = 50;
-            // 
-            // orderClient
-            // 
-            this.orderClient.HeaderText = "Client";
-            this.orderClient.Name = "orderClient";
-            // 
-            // orderCode
-            // 
-            this.orderCode.HeaderText = "Code";
-            this.orderCode.Name = "orderCode";
             // 
             // buttonLoadAgents
             // 
             this.buttonLoadAgents.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLoadAgents.Location = new System.Drawing.Point(303, 594);
+            this.buttonLoadAgents.Location = new System.Drawing.Point(291, 630);
             this.buttonLoadAgents.Margin = new System.Windows.Forms.Padding(2);
             this.buttonLoadAgents.Name = "buttonLoadAgents";
             this.buttonLoadAgents.Size = new System.Drawing.Size(97, 36);
@@ -134,7 +98,7 @@
             // buttonLoadOrders
             // 
             this.buttonLoadOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLoadOrders.Location = new System.Drawing.Point(1099, 594);
+            this.buttonLoadOrders.Location = new System.Drawing.Point(980, 630);
             this.buttonLoadOrders.Margin = new System.Windows.Forms.Padding(2);
             this.buttonLoadOrders.Name = "buttonLoadOrders";
             this.buttonLoadOrders.Size = new System.Drawing.Size(97, 36);
@@ -158,18 +122,115 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(1089, 13);
+            this.label2.Location = new System.Drawing.Point(975, 22);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "Orders";
             // 
-            // mainForm
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 547);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
+            // button_Previous_Agent
+            // 
+            this.button_Previous_Agent.Location = new System.Drawing.Point(160, 505);
+            this.button_Previous_Agent.Name = "button_Previous_Agent";
+            this.button_Previous_Agent.Size = new System.Drawing.Size(75, 23);
+            this.button_Previous_Agent.TabIndex = 14;
+            this.button_Previous_Agent.Text = "Previous";
+            this.button_Previous_Agent.UseVisualStyleBackColor = true;
+            this.button_Previous_Agent.Click += new System.EventHandler(this.button_Previous_Agent_Click);
+            // 
+            // button_Next_Agent
+            // 
+            this.button_Next_Agent.Location = new System.Drawing.Point(453, 505);
+            this.button_Next_Agent.Name = "button_Next_Agent";
+            this.button_Next_Agent.Size = new System.Drawing.Size(75, 23);
+            this.button_Next_Agent.TabIndex = 15;
+            this.button_Next_Agent.Text = "Next";
+            this.button_Next_Agent.UseVisualStyleBackColor = true;
+            this.button_Next_Agent.Click += new System.EventHandler(this.button_Next_Agent_Click);
+            // 
+            // labelPageNumberAgent
+            // 
+            this.labelPageNumberAgent.AutoSize = true;
+            this.labelPageNumberAgent.Location = new System.Drawing.Point(325, 515);
+            this.labelPageNumberAgent.Name = "labelPageNumberAgent";
+            this.labelPageNumberAgent.Size = new System.Drawing.Size(35, 13);
+            this.labelPageNumberAgent.TabIndex = 16;
+            this.labelPageNumberAgent.Text = "label4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            // 
+            // button_Previous_Order
+            // 
+            this.button_Previous_Order.Location = new System.Drawing.Point(811, 503);
+            this.button_Previous_Order.Name = "button_Previous_Order";
+            this.button_Previous_Order.Size = new System.Drawing.Size(75, 26);
+            this.button_Previous_Order.TabIndex = 17;
+            this.button_Previous_Order.Text = "Previous";
+            this.button_Previous_Order.UseVisualStyleBackColor = true;
+            this.button_Previous_Order.Click += new System.EventHandler(this.button_Previous_Order_Click);
+            // 
+            // button_Next_Order
+            // 
+            this.button_Next_Order.Location = new System.Drawing.Point(1116, 502);
+            this.button_Next_Order.Name = "button_Next_Order";
+            this.button_Next_Order.Size = new System.Drawing.Size(79, 26);
+            this.button_Next_Order.TabIndex = 18;
+            this.button_Next_Order.Text = "Next";
+            this.button_Next_Order.UseVisualStyleBackColor = true;
+            this.button_Next_Order.Click += new System.EventHandler(this.button_Next_Order_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 0;
+            // 
+            // labelPageNumberOrders
+            // 
+            this.labelPageNumberOrders.AutoSize = true;
+            this.labelPageNumberOrders.Location = new System.Drawing.Point(992, 510);
+            this.labelPageNumberOrders.Name = "labelPageNumberOrders";
+            this.labelPageNumberOrders.Size = new System.Drawing.Size(35, 13);
+            this.labelPageNumberOrders.TabIndex = 19;
+            this.labelPageNumberOrders.Text = "label5";
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(994, 646);
+            this.ClientSize = new System.Drawing.Size(1267, 709);
+            this.Controls.Add(this.labelPageNumberOrders);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_Next_Order);
+            this.Controls.Add(this.button_Previous_Order);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelPageNumberAgent);
+            this.Controls.Add(this.button_Next_Agent);
+            this.Controls.Add(this.button_Previous_Agent);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonLoadOrders);
@@ -178,7 +239,7 @@
             this.Controls.Add(this.ordersGridView);
             this.Controls.Add(this.agentsGridView);
             this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "mainForm";
+            this.Name = "FormMain";
             this.Text = "General Electric Services";
             this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.agentsGridView)).EndInit();
@@ -197,12 +258,17 @@
         private System.Windows.Forms.Button buttonLoadOrders;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn agentId;
-        private System.Windows.Forms.DataGridViewTextBoxColumn agentName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn serviciosXagente;
-        private System.Windows.Forms.DataGridViewTextBoxColumn orderId;
-        private System.Windows.Forms.DataGridViewTextBoxColumn orderClient;
-        private System.Windows.Forms.DataGridViewTextBoxColumn orderCode;
+        private System.Windows.Forms.Label label3;
+    
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_Previous_Agent;
+        private System.Windows.Forms.Button button_Next_Agent;
+        private System.Windows.Forms.Label labelPageNumberAgent;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_Previous_Order;
+        private System.Windows.Forms.Button button_Next_Order;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelPageNumberOrders;
     }
 }
 
--- GeneralElectric/Gui/Forms/FormMain.cs
+++ GeneralElectric/Gui/Forms/FormMain.cs
@@ -2,12 +2,17 @@
 {
     using System;
     using System.Windows.Forms;
+    using System.Threading.Tasks;
     using System.Linq;
     using Logic;
     using Logic.Classes;
+    using PagedList;
 
     public partial class FormMain : Form
     {
+        int pageNumber = 1;
+        IPagedList<Agent> AgentsList;
+        IPagedList<Order> OrdersList;
         private Context currentContext;
         private GeneticAllocator geneticAllocator;
 
@@ -29,25 +34,123 @@
 
         }
 
-        private void ButtonLoadAgents_Click(object sender, System.EventArgs e)
+
+        public async Task<IPagedList<Agent>> GetPagedAgentsList(int pageNumber = 1, int pageSize = 12)
         {
-            foreach (Agent agent in this.currentContext.Agents)
+
+            return await Task.Factory.StartNew(() =>
             {
-                this.agentsGridView.Rows.Add(agent.ID, agent.Name, string.Join(", ", agent.ServicesCodes));
-            }
+                return this.currentContext.Agents.ToPagedList(pageNumber, pageSize);
+
+
+
+                //return this.agentsGridView.Rows.Add(agent.ID, agent.Name, string.Join(", ", agent.ServicesCodes));
+
+            });
         }
 
-        private void buttonLoadOrders_Click(object sender, System.EventArgs e)
+
+        public async Task<IPagedList<Order>> GetPagedOrdersList(int pageNumber = 1, int pageSize = 12){
+
+              return await Task.Factory.StartNew(() =>
+              {
+                  return this.currentContext.Orders.ToPagedList(pageNumber,pageSize);
+
+              });
+          }
+
+        private async void ButtonLoadAgents_Click(object sender, System.EventArgs e)
         {
-            foreach (Order order in this.currentContext.Orders)
+            /* (Agent agent in this.currentContext.Agents)
+            {
+                this.agentsGridView.Rows.Add(agent.ID, agent.Name, string.Join(", ", agent.ServicesCodes));
+            }*/
+
+            AgentsList = await GetPagedAgentsList();
+            button_Previous_Agent.Enabled = AgentsList.HasPreviousPage;
+            button_Next_Agent.Enabled = AgentsList.HasNextPage;
+            agentsGridView.DataSource = AgentsList.ToList();
+            labelPageNumberAgent.Text = string.Format("Page {0}/{1}", pageNumber, AgentsList.PageCount);
+
+        }
+
+        private async void buttonLoadOrders_Click(object sender, System.EventArgs e)
+        {
+            /*foreach (Order order in this.currentContext.Orders)
             {
                 ordersGridView.Rows.Add(order.ID, order.Client, order.Service.Code);
-            }
+            }*/
+
+
+
+            OrdersList = await GetPagedOrdersList();
+            button_Previous_Order.Enabled = OrdersList.HasPreviousPage;
+            button_Next_Order.Enabled = OrdersList.HasNextPage;
+            ordersGridView.DataSource = OrdersList.ToList();
+            labelPageNumberOrders.Text = string.Format("Page {0}/{1}", pageNumber, OrdersList.PageCount);
         }
 
         private void FormMain_Load(object sender, EventArgs e)
         {
 
         }
+
+        private async void button_Next_Agent_Click(object sender, EventArgs e)
+        {
+
+            if (AgentsList.HasNextPage)
+            {
+                AgentsList = await GetPagedAgentsList(++pageNumber);
+                button_Previous_Agent.Enabled = AgentsList.HasPreviousPage;
+                button_Next_Agent.Enabled = AgentsList.HasNextPage;
+                agentsGridView.DataSource = AgentsList.ToList();
+                labelPageNumberAgent.Text = string.Format("Page {0}/{1}", pageNumber, AgentsList.PageCount);
+            }
+        }
+
+        private async void button_Previous_Order_Click(object sender, EventArgs e)
+        {
+
+            if (OrdersList.HasPreviousPage)
+            {
+                OrdersList = await GetPagedOrdersList(--pageNumber);
+                button_Previous_Order.Enabled = OrdersList.HasPreviousPage;
+                button_Next_Order.Enabled = OrdersList.HasNextPage;
+                ordersGridView.DataSource = OrdersList.ToList();
+                labelPageNumberOrders.Text = string.Format("Page {0}/{1}", pageNumber, OrdersList.PageCount);
+            }
+
+        }
+
+        private async void button_Next_Order_Click(object sender, EventArgs e)
+        {
+
+            if (OrdersList.HasNextPage)
+            {
+
+                OrdersList = await GetPagedOrdersList(++pageNumber);
+                button_Previous_Order.Enabled = OrdersList.HasPreviousPage;
+                button_Next_Order.Enabled = OrdersList.HasNextPage;
+                ordersGridView.DataSource = OrdersList.ToList();
+                labelPageNumberOrders.Text = string.Format("Page {0}/{1}", pageNumber, OrdersList.PageCount);
+            }
+        }
+
+        private async void button_Previous_Agent_Click(object sender, EventArgs e)
+        {
+
+            if (AgentsList.HasPreviousPage)
+            {
+                AgentsList = await GetPagedAgentsList(--pageNumber);
+                button_Previous_Agent.Enabled = AgentsList.HasPreviousPage;
+                button_Next_Agent.Enabled = AgentsList.HasNextPage;
+                agentsGridView.DataSource = AgentsList.ToList();
+                labelPageNumberAgent.Text = string.Format("Page {0}/{1}", pageNumber, AgentsList.PageCount);
+            }
+        }
     }
+    
 }
+        
+    
+
